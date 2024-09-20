1942548142ecdef481ac1c5bd0921052aa83a918
Sudheendra Kumar Kaanugovi <sudhy.k@gmail.com>
2024-09-19 21:42:12 -0400

    Change Proj Name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Whisper",
+    name: "MyWhisper",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Whisper",
-            targets: ["Whisper"]),
+            name: "MyWhisper",
+            targets: ["MyWhisper"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Whisper",
+            name: "MyWhisper",
             cSettings: [
                 .define("GGML_USE_ACCELERATE", .when(platforms: [.macOS, .macCatalyst, .iOS])),
                 .define("WHISPER_USE_COREML", .when(platforms: [.macOS, .macCatalyst, .iOS])),
